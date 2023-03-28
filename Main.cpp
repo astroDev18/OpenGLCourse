ffd92ce6c36e606557c1393b91ce84de8e028631
Isa Ali <isajaffarali@gmail.com>
2023-03-28 07:07:00 +0300

    Refactored code to use shader loader shader_s.h.

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,15 @@
-#include"imgui.h"
-#include"imgui_impl_glfw.h"
-#include"imgui_impl_opengl3.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
+
+#include <iostream>
+#include "shader_s.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-// Declare our vertex shader
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"uniform float size;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(size * aPos.x, size * aPos.y, size * aPos.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"uniform vec4 color;\n"
-"void main()\n"
-"{\n"
-"   FragColor = color;\n"
-
-"}\n\0";
-
 
 int main()
 {
@@ -74,197 +56,65 @@ int main()
 	glViewport(0, 0, 800, 800);
 
 
-	// Build and compile our Shader program
-	// <----------------------------------->
-	// Declare a shader object
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-	// Attatch the shader source code to the shader object and compile the shader
-	// Shader to compile; Number of Strings; Source code of vertex shader (stores as variable); Length (Null here);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	// Verify successful compilation
-	// Int to indicate success
-	int success;
-	// Container for error messages
-	char infoLog[512];
-	// use glGetShaderiv to see if compilation is successful
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-	// Error message, retrieved with glGetShaderInfoLog
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	// Compile a fragment shader
-	// Similar process, but using a GL_FRAGMENT_SHADER as shader type 
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	// Error check
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-
-	// Shader Program
-	// It is the final linked version of multiple shaders combined
-	// Activate it when rendering objects at render calls
-	unsigned int shaderProgram;
-	// Creates a program and returns the ID reference to the newly created program object
-	shaderProgram = glCreateProgram();
-	// Attatch previously compiled shaders to the program object and then link with glLinkProgram
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	// build and compile our shader program
+	// ------------------------------------
+	Shader ourShader("default.vert", "default.frag"); 
+	// Vertices coordinates
+	float vertices[] = {
+		// positions         // colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
 	};
 
-	// Delete shaders, no longer needed after linking
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-
-
-	// Specify the color of the background
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	// Clean the back buffer and assign the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-	// Swap the back buffer with the front buffer
-	glfwSwapBuffers(window);
-
-	// Storing vertex data within memory on the graphics card by setting up Vertex data Buffer
-	// <------------------------------------------------------>
-	// Input Vertex Data, handled in 3d x, y, and z.
-
-
-	GLfloat vertices[] = {
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
-	};
-
-	unsigned int indices[] = {
-		0, 1, 2, // First triangle
-	};
-
-	// VBO is used to store large amounts of data to be sent to the GPU's memory
-	// without having to send each vertex one at a time
-	GLuint VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &EBO);
 	glGenBuffers(1, &VBO);
-	// Bind our VAO, then bind and set vertex buffer, and then config vertex attributes
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO);
 
-	// Bind the buffer to GL_ARRAY_BUFFER with glBindBuffer
-	// Copies our vertices array into a vertex buffer for OpenGL's use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// Function copies previously defined vertex data into the currently bound buffer's memory
-	// Type of buffer; size of data; data to send; graphics management type, here static as the data will not change;
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	// set vertix data interpretation before render
-	// 1. Vertix attribute to configure (0); 2. size of attribute (vec3); 3. data type (float); normalization (false); 
-	// 4. Stride, which is space between vertix attributes (3), 5. Data position offset (void);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	// Initialize ImGui
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	ImGui::StyleColorsDark();
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
 
-	// ImGui Variables
-	bool drawTriangle = true;
-	float size = 1.0f;
-	float color[4] = { 0.8f, 0.3f, 0.02f, 1.0f };
-
-	// Exporting variables to shaders
-	glUseProgram(shaderProgram);
-	glUniform1f(glGetUniformLocation(shaderProgram, "size"), size);
-	glUniform4f(glGetUniformLocation(shaderProgram, "color"), color[0], color[1], color[2], color[3]);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	// Main while loop
+	// render loop
+	// -----------
 	while (!glfwWindowShouldClose(window))
 	{
-		// Key Input
+		// input
+		// -----
 		processInput(window);
 
-		// Render
+		// render
+		// ------
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-
-
-		// Draw our first triangle
-		glUseProgram(shaderProgram);
+		// render the triangle
+		ourShader.use();
 		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		// Render elements from our index buffer
-		// Only draw triangle if ImGui is checked
-		if (drawTriangle)
-			// 1. Drawing mode; 2. Count of elements to draw; 3. Type of indices; 4. Offset in EBO;
-			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		// ImGui Window Creation
-		ImGui::Begin("My first ImGui Window!");
-		// Text that appears
-		ImGui::Text("Welcome to ImGui");
-		// Checkbox
-		ImGui::Checkbox("Draw Triangle", &drawTriangle);
-		// Slider that appears in window
-		ImGui::SliderFloat("Size", &size, 0.5f, 2.0f);
-		// Fancy color editor
-		ImGui::ColorEdit4("Color", color);
-		ImGui::End();
-
-		glUseProgram(shaderProgram);
-		glUniform1f(glGetUniformLocation(shaderProgram, "size"), size);
-		glUniform4f(glGetUniformLocation(shaderProgram, "color"), color[0], color[1], color[2], color[3]);
-
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-		// Take care of all GLFW events
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-
-
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
-	// Delete window before ending the program
-	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
 }
